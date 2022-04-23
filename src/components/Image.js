7755177e865dc321e306020392bf6b10ea9ec273
Ramkalyan7 <yramkalyan0707@gmail.com>
2022-04-23 15:28:23 +0530

    fixed border of image

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,11 +3,11 @@ import React from "react";
 const Image = ({ imgUrl }) => {
   return (
     <>
-      <div className="row-item      hover:scale-110  hover:z-10 hover:border-4 active:border-4 active:border-red-700 border-white transition-all">
+      <div className="row-item  ">
         <img
           src={imgUrl}
           alt="moivieimage"
-          className="max-w-none w-[98%]  rounded-lg"
+          className="max-w-none w-[98%]  rounded-lg     hover:scale-125  hover:z-10 hover:border-4 active:border-4 active:border-red-700 border-white transition-all"
         />
       </div>
     </>
